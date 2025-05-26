43eb14b11a1426eeca9b033f9fb85241e1f05c32
Alexander Liu <a@alexanderliu.com>
2025-05-25 21:05:19 -0700

    feat: default response

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+	return c.text("Hello! This is the ICSSC Projects integration server.");
 });
 
 export default app;
